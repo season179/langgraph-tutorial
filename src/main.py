7e5c770192ed3219b9601ee39a50bf8a16d5fa66
Season Saw <season.saw@gmail.com>
2024-06-18 15:36:40 +0800

    Could not get tools to work. Suspect it is because of the custom tool node.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,48 @@
-import getpass
-import os
+import json
 from dotenv import load_dotenv, find_dotenv
-from typing import Annotated
+from typing import Annotated, Literal
 from typing_extensions import TypedDict
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
+from langchain_core.messages import BaseMessage, ToolMessage
 from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
 
 
 load_dotenv(find_dotenv())
+
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+
 llm = ChatAnthropic(model="claude-3-haiku-20240307")
+# Modification: tell the LLM which tools it can call
+llm_with_tools = llm.bind_tools(tools)
+
+
+class BasicToolNode:
+    """A node that runs the tools requested in the last AIMessage."""
+
+    def __init__(self, tools: list) -> None:
+        self.tools_by_name = {tool.name: tool for tool in tools}
+
+    def __call__(self, inputs: dict):
+        if messages := inputs.get("messages", []):
+            message = messages[-1]
+        else:
+            raise ValueError("No message found in input")
+        outputs = []
+        for tool_call in message.tool_calls:
+            tool_result = self.tools_by_name[tool_call["name"]].invoke(
+                tool_call["args"]
+            )
+            outputs.append(
+                ToolMessage(
+                    content=json.dumps(tool_result),
+                    name=tool_call["name"],
+                    tool_call_id=tool_call["id"],
+                )
+            )
+        return {"messages": outputs}
 
 
 class State(TypedDict):
@@ -17,7 +50,7 @@ class State(TypedDict):
     # in the annotation defines how this state key should be updated
     # (in this case, it appends messages to the list, rather than overwriting them)
     messages: Annotated[list, add_messages]
-    
+
 
 graph_builder = StateGraph(State)
 
@@ -26,24 +59,62 @@ def chatbot(state: State):
     return {"messages": [llm.invoke(state["messages"])]}
 
 
+def route_tools(state: State) -> Literal["tools", "__end__"]:
+    """Use in the conditional_edge to route to the ToolNode if the last message
+
+    has tool calls. Otherwise, route to the end."""
+
+    if isinstance(state, list):
+        ai_message = state[-1]
+    elif messages := state.get("messages", []):
+        ai_message = messages[-1]
+    else:
+        raise ValueError(f"No messages found in input state to tool_edge: {state}")
+
+    if hasattr(ai_message, "tool_calls") and len(ai_message.tool_calls) > 0:
+        return "tools"
+
+    return "__end__"
+
+
 # The first argument is the unique node name
 # The second argument is the function or object that will be called whenever
 # the node is used.
 graph_builder.add_node("chatbot", chatbot)
 
+tool_node = BasicToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+# The `tools_condition` function returns "tools" if the chatbot asks to use a tool, and "__end__" if
+# it is fine directly responding. This conditional routing defines the main agent loop.
+graph_builder.add_conditional_edges(
+    "chatbot",
+    route_tools,
+    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+    # It defaults to the identity function, but if you
+    # want to use a node named something else apart from "tools",
+    # You can update the value of the dictionary to something else
+    # e.g., "tools": "my_tools"
+    {"tools": "tools", "__end__": "__end__"},
+)
+
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+
 graph_builder.set_entry_point("chatbot")
 
-graph_builder.set_finish_point("chatbot")
+# graph_builder.set_finish_point("chatbot")
 
 graph = graph_builder.compile()
 
 while True:
     user_input = input("User: ")
-    
+
     if user_input.lower() in ["quit", "exit", "q"]:
-        print("Goodbye")
+        print("Goodbye!")
         break
-    
-    for event in graph.stream({"messages": ("user", user_input)}):
+
+    for event in graph.stream({"messages": [("user", user_input)]}):
         for value in event.values():
-            print("Assistant: ", value["messages"][-1].content)
+            if isinstance(value["messages"][-1], BaseMessage):
+                print("Assistant:", value["messages"][-1].content)
